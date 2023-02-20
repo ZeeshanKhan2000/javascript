cfabf4e0ce55301c93f2710d9efdf25802e5e4e0
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-02-20 19:28:17 +0200

    Update class-2.js

--- class-2.js
+++ class-2.js
@@ -1,1 +1,27 @@
 
+// class no 2  
+
+
+
+// <script> 
+//   alert('hello world')
+// document.write('zeeshan')
+//  </script>
+
+
+// html section
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>javascript basic class no 2</h1>
+//     <p>data types</p>
+//     <script src="class.js"></script>
+// </body>
+// </html>
