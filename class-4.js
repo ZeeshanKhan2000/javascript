f98024292a660d178e9324ac513afa2e895e742b
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-02-21 00:15:56 +0200

    Update class-4.js

--- class-4.js
+++ class-4.js
@@ -3,58 +3,66 @@
 
 <!-- <h1>java script array class no 4</h1> -->
 
-// Topic no 4  javascript arrays  method  
+Topic no 4  javascript arrays  method  
 
 
 // arrays method 
-// (01) .join
-// (02) .concat 
-// (03) .PUSH
-// (04) .pop
-// (05) .unshift
-// (06) .shift
-// (07) .indexof
 
-// let name = [ 'zeeshan', 'adil', 'ahsan', 'damil',];
-// let diff = ['ali', 12 , true];
+(01) .join
+(02) .concat 
+(03) .PUSH
+(04) .pop
+(05) .unshift
+(06) .shift
+(07) .indexof
+
+let name = [ 'zeeshan', 'adil', 'ahsan', 'damil',];
+let diff = ['ali', 12 , true];
 
 
 
 // (01)
-// to update or replace  the value or change the value in arrays
-// name[2] = 'faraz'      
 
-//console.log( name.join(',')  );  //, join with all the varaible 
-//console.log( name.join('=')  );  //, join with all the varaible
-//console.log( name.join('+')  );  //, join with all the varaible any thing you put in the .join it will add in the value
+to update or replace  the value or change the value in arrays
+name[2] = 'faraz'      
+
+console.log( name.join(',')  );  //, join with all the varaible 
+console.log( name.join('=')  );  //, join with all the varaible
+console.log( name.join('+')  );  //, join with all the varaible any thing you put in the .join it will add in the value
 
 
-// (02)  arrays methods 
- //.concat use for to add the more value or sum text in End of  array   
-// console.log( name.concat([true , 123 ,'hellow'])); 
+// (02)  
+
+   arrays methods 
+.concat use for to add the more value or sum text in End of  array   
+console.log( name.concat([true , 123 ,'hellow'])); 
 
 
 // (03)
-// .push use for to add the  more value like ather values in End of  array 
-// console.log( name.push('jibran'));
+
+.push use for to add the  more value like ather values in End of  array 
+console.log( name.push('jibran'));
 
 // (04)
-//  .pop use for cuting or  removing the last value of the array  
-// console.log( name.pop());
-// console.log( name.pop());
+
+ .pop use for cuting or  removing the last value of the array  
+console.log( name.pop());
+console.log( name.pop());
 
 // (05)
-// .unshift add a value in (0) index or add the value in the start of array first value 
-// console.log( name.pop());
-// console.log( name.unshift('zarar', 'talha'));
+
+.unshift add a value in (0) index or add the value in the start of array first value 
+console.log( name.pop());
+console.log( name.unshift('zarar', 'talha'));
 
 
 // (06)
-// .shift remove or delete a value in (0) index or delete the value in the start of array first value 
-// console.log( name.shift());  
+
+.shift remove or delete a value in (0) index or delete the value in the start of array first value 
+console.log( name.shift());  
 
  
-// console.log( name);
+console.log( name);
 
 
 
