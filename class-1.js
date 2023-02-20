84ee3425c6062cbc92f3a85bfd37befbe40d4244
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-02-20 19:02:08 +0200

    Update class-1.js

--- class-1.js
+++ class-1.js
@@ -1,1 +1,13 @@
-
+ // download vs code 
+// extentions vs code  
+ 
+// live server 
+// material icon them
+// auto close tag
+// auto rename tag
+// auto import ES6 jsx ts Tsx
+// beracket pair colorizer
+// html css support
+// javascript snippets
+// prettier code formater
+// node js
