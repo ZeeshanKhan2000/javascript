5971c433434767cdfaaaf9e26c3e4cb40bf0add6
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-02-20 20:29:40 +0200

    Update class-3.js

--- class-3.js
+++ class-3.js
@@ -1,1 +1,45 @@
 
+// <h1>javascript Data Types class no 3</h1>
+ 
+// <P>there are 5 types of Data Types  in javascript </ P>
+
+// alert('hello world')
+// document.write('zeeshan')
+
+
+// alert('hello class');
+
+// var name = prompt('wellcome to javasrcipt');
+// console.log(neme);
+
+// difference between the 
+
+// var   use globaly
+// let   not use globaly use olny when need
+// const  use only for the specific values which can not be change for the 
+
+
+// data types 
+// 1 premitive
+    // (01) string   use for character in always in side the ""
+    // (02) number  use for the value  without ""
+    // (03) boolean condition false or true 
+    // (04) null  a  empty variable
+    // (05) undefine value
+     
+    // (01) string example   with  // Concatenating text strings
+
+    //   let name = 'zeeshan'
+    //   let age = 21 
+    //   let profession = 'developer'
+    //   let exp = '4 year experience'
+    //   let result = "i am " + name + " afzal and my age  is " + age +  " and i am working as a web " + profession + " having  a " + exp +  " resume ";
+      
+    //   console.log(result.toUpperCase());
+    //   console.log(result.toLowerCase());
+    //   console.log(result.length);
+    //   console.log(result[6]);
+    //   console.log(result.slice(0,5)); use to count the charcter  from index
+     //  console.log(result.substr());  use to find out the character from the exate number and count all the given number  
+    //   console.log(result);
+
