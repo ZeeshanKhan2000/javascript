d439b156baa5ee9eba2928c20d60107fc96e4eb2
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2024-09-06 01:41:55 +0500

    Create object.js

---
+++ object.js
@@ -0,0 +1,56 @@
+// non premitive data typess 
+
+An object literal is a list of property and we can use group of data and  defferent data types 
+  names:values inside curly braces {}.
+
+const studnets = {
+  firstName:"John",
+    lastName:"Doe", 
+    age:50,
+    eyeColor:"blue"
+};
+
+console.log(student)
+
+// we can also declare single variable like this ;
+
+console.log(student.age)
+// or 
+console.log(student[age]) 
+
+// more example 
+// about products 
+
+const product = {
+  prodctTtle = "pens";
+  rating = 3 ;
+  offer = true /false ;
+  productDiscription = "xyz"
+  
+}
+console.log(product) ;
+
+console.log(typeof product["offer"]) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// and to chhange the variable value 
+studnet["age"] + studnet["age"] + 1;
+// for name 
+studnet["name"] + "zeeshan"
+
